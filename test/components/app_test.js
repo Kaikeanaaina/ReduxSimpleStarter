6306d671a541a07ed67f6d92fbb38130af1806e7
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-04-30 14:13:06 -1000

    updated

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,19 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// use 'describe' to group together similar tests
 describe('App' , () => {
   let component;
 
   beforeEach(() => {
+    // create an instance of App
     component = renderComponent(App);
   });
-
+  
+  // use 'it' to test a single attribute of a target
   it('renders something', () => {
+    //use 'expect' to make an 'assertion' about a target
     expect(component).to.exist;
+    expect(component).to.contain('React simple starter')
   });
 });
